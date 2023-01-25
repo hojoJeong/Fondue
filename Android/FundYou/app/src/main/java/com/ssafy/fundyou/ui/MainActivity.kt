d116c8fbc201550981dbb3d8fa285e9df0fd0f98
RyuSw-cs <rsw1452@naver.com>
2023-01-25 13:48:08 +0900

    chore: #1 폰트 추가 및 앱바 삭제

--- Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/MainActivity.kt
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/MainActivity.kt
@@ -3,10 +3,15 @@ package com.ssafy.fundyou.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ssafy.fundyou.R
+import com.ssafy.fundyou.databinding.ActivityBridgeBinding
+import com.ssafy.fundyou.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
