fbb71e62b1b02980d09867d3028d236804ed71c4
RyuSw-cs <rsw1452@naver.com>
2023-01-27 17:59:38 +0900

    feat: #6 SearchViewModel 구현

--- Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/search/SearchViewModel.kt
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/search/SearchViewModel.kt
@@ -1,4 +1,70 @@
 package com.ssafy.fundyou.ui.search
 
-class SearchViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.fundyou.common.Constant.SUCCESS
+import com.ssafy.fundyou.common.ViewState
+import com.ssafy.fundyou.domain.usecase.search.DeleteAllHistoryKeywordUseCase
+import com.ssafy.fundyou.domain.usecase.search.DeleteSearchHistoryKeywordUseCase
+import com.ssafy.fundyou.domain.usecase.search.GetSearchHistoryListUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val getSearchHistoryListUseCase: GetSearchHistoryListUseCase,
+    private val deleteSearchHistoryKeywordUseCase: DeleteSearchHistoryKeywordUseCase,
+    private val deleteAllHistoryKeywordUseCase: DeleteAllHistoryKeywordUseCase
+) : ViewModel() {
+    private val _searchKeywordList = MutableLiveData<ViewState<ArrayList<String>>>()
+    val searchKeywordList: LiveData<ViewState<ArrayList<String>>> get() = _searchKeywordList
+
+    private val _allRemoveState = MutableLiveData<ViewState<Int>>()
+    val allRemoveState: LiveData<ViewState<Int>> get() = _allRemoveState
+
+    private val _keywordRemoveState = MutableLiveData<ViewState<Int>>()
+    val keywordRemoveState: LiveData<ViewState<Int>> get() = _keywordRemoveState
+
+    fun getSearchKeywordList() = viewModelScope.launch {
+        _searchKeywordList.value = ViewState.Loading()
+        try {
+            val response = getSearchHistoryListUseCase()
+            _searchKeywordList.value = ViewState.Success(response)
+        } catch (e: Exception) {
+            _searchKeywordList.value = ViewState.Error(e.message)
+        }
+    }
+
+    fun deleteAllKeyword() =
+        viewModelScope.launch {
+            _keywordRemoveState.value = ViewState.Loading()
+            try {
+                val response = deleteAllHistoryKeywordUseCase()
+                if (response == SUCCESS) {
+                    _keywordRemoveState.value = ViewState.Success(response)
+                } else {
+                    _keywordRemoveState.value = ViewState.Error("error")
+                }
+            } catch (e: Exception) {
+                _keywordRemoveState.value = ViewState.Error(e.message)
+            }
+        }
+
+    fun deleteSearchKeyword(baseList: ArrayList<String>, keywordIndex: Int) =
+        viewModelScope.launch {
+            _allRemoveState.value = ViewState.Loading()
+            try {
+                val response = deleteSearchHistoryKeywordUseCase(baseList, keywordIndex)
+                if (response == SUCCESS) {
+                    _allRemoveState.value = ViewState.Success(response)
+                } else {
+                    _allRemoveState.value = ViewState.Error("error")
+                }
+            } catch (e: Exception) {
+                _allRemoveState.value = ViewState.Error(e.message)
+            }
+        }
 }
--- Android/FundYou/data/src/main/java/com/ssafy/fundyou/data/remote/repository/SearchRepositoryImpl.kt
+++ Android/FundYou/data/src/main/java/com/ssafy/fundyou/data/remote/repository/SearchRepositoryImpl.kt
@@ -1,5 +1,9 @@
 package com.ssafy.fundyou.data.remote.repository
 
+import android.os.Build.VERSION_CODES.S
+import com.ssafy.fundyou.common.Constant
+import com.ssafy.fundyou.common.Constant.SP_ERROR
+import com.ssafy.fundyou.common.Constant.SUCCESS
 import com.ssafy.fundyou.data.local.prefs.SearchKeywordPreference
 import com.ssafy.fundyou.domain.repository.SearchRepository
 import javax.inject.Inject
@@ -7,18 +11,18 @@ import javax.inject.Inject
 class SearchRepositoryImpl @Inject constructor(
     private val searchKeywordPreference: SearchKeywordPreference
 ) : SearchRepository {
-    override suspend fun getHistoryKeyword(): List<String> {
+    override suspend fun getHistoryKeyword(): ArrayList<String> {
         return searchKeywordPreference.getKeywordList()
     }
 
     override suspend fun deleteHistoryKeyword(baseList: ArrayList<String>, index: Int): Int {
         val baseSize = baseList.size
         searchKeywordPreference.removeKeyword(baseList, index)
-        return if (searchKeywordPreference.getKeywordList().size == baseSize) 0 else 1
+        return if (searchKeywordPreference.getKeywordList().size == baseSize) SP_ERROR else SUCCESS
     }
 
     override suspend fun deleteAllHistoryKeyword(): Int {
         searchKeywordPreference.removeAllKeyword()
-        return if (getHistoryKeyword().isEmpty()) 0 else 1
+        return if (getHistoryKeyword().isEmpty()) SP_ERROR else SUCCESS
     }
 }
