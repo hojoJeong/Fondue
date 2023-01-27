a1ffedd312aad48c957afd6754ceca67e2679864
hojo0423 <hojo0423@naver.com>
2023-01-27 18:01:00 +0900

    feat: #3 메인 베너 수정, 카테고리 구현

---
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/MainCategoryAdapter.kt
@@ -0,0 +1,44 @@
+package com.ssafy.fundyou
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.fundyou.databinding.ItemMainCategoryBinding
+import com.ssafy.fundyou.ui.MainFragment
+
+class MainCategoryAdapter : RecyclerView.Adapter<MainCategoryAdapter.MainCategoryViewHolder>() {
+    private var categoryItemList = mutableListOf<MainCategoryModel>()
+    private lateinit var activity: MainFragment
+
+    inner class MainCategoryViewHolder(val binding: ItemMainCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: MainCategoryModel) {
+            binding.category = item
+            binding.activity = activity
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainCategoryViewHolder {
+        val view = DataBindingUtil.inflate<ItemMainCategoryBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_main_category,
+            parent,
+            false
+        )
+        return MainCategoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MainCategoryViewHolder, position: Int) {
+        holder.bind(categoryItemList[position])
+    }
+
+    override fun getItemCount(): Int = categoryItemList.size
+
+    fun initCategoryItem(list: MutableList<MainCategoryModel>, context: MainFragment){
+        categoryItemList.clear()
+        categoryItemList.addAll(list)
+        activity = context
+    }
+}
---
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/MainCategoryModel.kt
@@ -0,0 +1,9 @@
+package com.ssafy.fundyou
+
+import android.graphics.drawable.Drawable
+
+data class MainCategoryModel(
+    val categoryImg: Drawable,
+    val categoryName: String
+) {
+}
