a7e792e8ecb401560d83128eab40af002332cc43
heejucherich <gmlwn1954@gmail.com>
2023-02-02 08:59:02 +0900

    feat: #1-환경설정 및 기능별 엔티티생성

---
+++ BE-develop/fundyou1/src/main/java/com/ssafy/fundyou1/Fundyou1Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.fundyou1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundyou1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundyou1Application.class, args);
+	}
+
+}
