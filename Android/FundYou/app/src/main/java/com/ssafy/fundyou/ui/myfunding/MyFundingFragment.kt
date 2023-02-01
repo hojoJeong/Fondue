0cadc4fe261522ec2cff79e4943d27318d325c7d
RyuSw-cs <rsw1452@naver.com>
2023-02-01 13:04:45 +0900

    feat: #13 내 펀딩 XML

---
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/myfunding/MyFundingFragment.kt
@@ -0,0 +1,16 @@
+package com.ssafy.fundyou.ui.myfunding
+
+import com.ssafy.fundyou.R
+import com.ssafy.fundyou.databinding.FragmentMyFundingBinding
+import com.ssafy.fundyou.ui.base.BaseFragment
+
+class MyFundingFragment : BaseFragment<FragmentMyFundingBinding>(R.layout.fragment_my_funding) {
+
+    override fun initView() {
+
+    }
+
+    override fun initViewModels() {
+
+    }
+}
