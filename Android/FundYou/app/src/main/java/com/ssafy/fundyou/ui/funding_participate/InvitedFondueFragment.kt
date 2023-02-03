7fbeb874fab5fa153390903d16771503ac6b5732
hojo0423 <hojo0423@naver.com>
2023-02-03 12:58:48 +0900

    feat: #12 초대받은 퐁듀 UI 개발 중

--- Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/funding_participate/InvitedFondueFragment.kt
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/funding_participate/InvitedFondueFragment.kt
@@ -5,58 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
 import com.ssafy.fundyou.R
 import com.ssafy.fundyou.databinding.FragmentInvitedFondueBinding
-import com.ssafy.fundyou.databinding.ItemInvitedFondueBinding
 import com.ssafy.fundyou.ui.base.BaseFragment
-import com.ssafy.fundyou.ui.funding_participate.model.InvitedFundingModel
 
 class InvitedFondueFragment : BaseFragment<FragmentInvitedFondueBinding>(R.layout.fragment_invited_fondue) {
-    private val itemList = mutableListOf<InvitedFundingModel>()
-    private val currentCategory = ""
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initView()
-    }
     override fun initView() {
-        initInvitedFondueCategory()
-        initInvitedFondueList()
+        TODO("Not yet implemented")
     }
 
     override fun initViewModels() {
-    }
-
-    private fun initInvitedFondueCategory(){
-
-    }
-
-    private fun initInvitedFondueList(){
-        //TODO(임시 데이터 추가)
-        with(itemList){
-            clear()
-            add(InvitedFundingModel(0, "김싸피", true, false, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", false, false, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", true, true, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", false, true, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", true, false, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", false, false, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", true, true, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", false, true, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", true, false, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", false, false, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", true, true, "2023.01.01 ~ 2023.02.01",1000))
-            add(InvitedFundingModel(0, "김싸피", false, true, "2023.01.01 ~ 2023.02.01",1000))
-
-        }
-
-        val invitedFondueAdapter = InvitedFondueItemAdapter()
-        invitedFondueAdapter.submitList(itemList)
-        with(binding.rvInvitedFondueItem){
-            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
-            adapter = invitedFondueAdapter
-        }
-
+        TODO("Not yet implemented")
     }
 
 }
--- Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/funding_participate/InvitedFondueListItemAdapter.kt
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/funding_participate/InvitedFondueListItemAdapter.kt
@@ -1,27 +1,24 @@
 package com.ssafy.fundyou.ui.funding_participate
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.fundyou.R
-import com.ssafy.fundyou.databinding.ItemInvitedFondueBinding
-import com.ssafy.fundyou.ui.adapter.diffutil.FundingItemModelDiffUtil
+import com.ssafy.fundyou.databinding.ItemInvitedFondueListBinding
 import com.ssafy.fundyou.ui.adapter.diffutil.InvitedFundingDiffUtil
 import com.ssafy.fundyou.ui.funding_participate.model.InvitedFundingModel
-import com.ssafy.fundyou.ui.myfunding.model.FundingItemModel
 
-class InvitedFondueItemAdapter : ListAdapter<InvitedFundingModel, InvitedFondueItemAdapter.InvitedFondueItemHolder>(InvitedFundingDiffUtil){
-    inner class InvitedFondueItemHolder(private val binding: ItemInvitedFondueBinding) : RecyclerView.ViewHolder(binding.root){
+class InvitedFondueListItemAdapter : ListAdapter<InvitedFundingModel, InvitedFondueListItemAdapter.InvitedFondueItemHolder>(InvitedFundingDiffUtil){
+    inner class InvitedFondueItemHolder(private val binding: ItemInvitedFondueListBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(item: InvitedFundingModel){
             binding.fundingItem = item
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InvitedFondueItemHolder {
-        val view = DataBindingUtil.inflate<ItemInvitedFondueBinding>(LayoutInflater.from(parent.context), R.layout.item_invited_fondue, parent, false)
+        val view = DataBindingUtil.inflate<ItemInvitedFondueListBinding>(LayoutInflater.from(parent.context), R.layout.item_invited_fondue_list, parent, false)
         return InvitedFondueItemHolder(view)
     }
 
