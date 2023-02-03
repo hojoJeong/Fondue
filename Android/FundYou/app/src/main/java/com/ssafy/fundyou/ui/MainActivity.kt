86caad6459a2c6e6e6eb8066628c668d37053cbe
RyuSw-cs <rsw1452@naver.com>
2023-02-03 22:42:05 +0900

    feat: #22 툴바 구현

--- Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/MainActivity.kt
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ssafy.fundyou.ui
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -34,17 +37,101 @@ class MainActivity : AppCompatActivity() {
         navController.addOnDestinationChangedListener { _, _, _ ->
             when (navController.currentDestination?.id) {
                 R.id.mainFragment -> {
-                    Log.d(TAG, "initNavigation: main")
+                    setToolbarType(ToolbarType.LOGO_WISH)
+                    setBottomNavigationVisibility(View.VISIBLE)
                 }
                 R.id.searchFragment -> {
-                    Log.d(TAG, "initNavigation: search")
+                    setToolbarType(ToolbarType.TEXT, "검색")
+                    setBottomNavigationVisibility(View.VISIBLE)
                 }
             }
         }
         binding.bnvMain.setupWithNavController(navController)
     }
 
+    private fun setBottomNavigationVisibility(visibility: Int){
+        binding.bnvMain.visibility = visibility
+    }
+
+    private fun setToolbarType(type : ToolbarType, title : String = ""){
+        when(type){
+            ToolbarType.LOGO_WISH -> {
+                setToolbarLeft(ToolbarLeftImg.LOGO)
+                setToolbarRight(ToolbarRightImg.WISH)
+            }
+            ToolbarType.BACK_TITLE_WISH -> {
+                setToolbarTitle(title)
+                setToolbarLeft(ToolbarLeftImg.BACK)
+                setToolbarRight(ToolbarRightImg.WISH)
+            }
+            ToolbarType.TEXT -> {
+                setToolbarTitle(title)
+            }
+            ToolbarType.TEXT_CANCEL -> {
+                setToolbarTitle(title)
+                setToolbarRight(ToolbarRightImg.CANCEL)
+            }
+            ToolbarType.BACK_TEXT -> {
+                setToolbarLeft(ToolbarLeftImg.BACK)
+                setToolbarTitle(title)
+            }
+        }
+    }
+
+    private fun setToolbarLeft(type : ToolbarLeftImg){
+        when(type){
+            ToolbarLeftImg.BACK -> {
+                with(binding.lyToolbar.ivLeftImg){
+                    setOnClickListener {
+                        navController.popBackStack()
+                    }
+                    setImageResource(R.drawable.ic_back_arrow_no_line)
+                }
+            }
+            ToolbarLeftImg.LOGO -> {
+                binding.lyToolbar.ivLeftImg.setImageResource(R.drawable.ic_launcher_background)
+            }
+        }
+    }
+
+    private fun setToolbarRight(type : ToolbarRightImg){
+        when(type){
+            ToolbarRightImg.WISH -> {
+                with(binding.lyToolbar.ivRightImg){
+                    setImageResource(R.drawable.ic_wish_list)
+                    setOnClickListener {
+
+                    }
+                }
+            }
+            ToolbarRightImg.CANCEL -> {
+                with(binding.lyToolbar.ivLeftImg){
+                    setImageResource(R.drawable.ic_cancel)
+                    setOnClickListener {
+                        navController.popBackStack()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setToolbarTitle(title : String){
+        binding.lyToolbar.tvTitle.text = title
+    }
+
     companion object {
         private const val TAG = "MainActivity..."
     }
+}
+
+enum class ToolbarType{
+    LOGO_WISH, BACK_TITLE_WISH, TEXT, TEXT_CANCEL, BACK_TEXT
+}
+
+enum class ToolbarLeftImg{
+    LOGO, BACK
+}
+
+enum class ToolbarRightImg{
+    WISH, CANCEL
 }
