6a4da83acca36e10f7682dc8bab93d17e7e354d1
RyuSw-cs <rsw1452@naver.com>
2023-02-08 13:59:28 +0900

    feat: # 딥링크 생성 완료

--- Android/FundYou/common/src/main/java/com/ssafy/fundyou/common/Constant.kt
+++ Android/FundYou/common/src/main/java/com/ssafy/fundyou/common/Constant.kt
@@ -1,13 +1,14 @@
 package com.ssafy.fundyou.common
 
 object Constant {
+    //http://172.26.12.151:22
     const val BASE_URL = "http://192.168.100.72:8080"
 
     const val SP_ERROR = 500
     const val SUCCESS = 200
 }
 
-object ErrorMessage{
+object ErrorMessage {
     const val NO_TOKEN = "NO_TOKEN"
     const val AUTHORIZATION_ERROR = "AUTHORIZATION_ERROR"
 }
