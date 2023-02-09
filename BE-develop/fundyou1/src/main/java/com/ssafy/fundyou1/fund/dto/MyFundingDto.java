51d1b7bc279e2053747a6bbbc258121d83ca1098
jihyun <skql8555@naver.com>
2023-02-09 13:42:37 +0900

    feat: #50 나의 진행중 & 종료된 펀딩 리스트 API 생성

---
+++ BE-develop/fundyou1/src/main/java/com/ssafy/fundyou1/fund/dto/MyFundingDto.java
@@ -0,0 +1,36 @@
+package com.ssafy.fundyou1.fund.dto;
+
+import com.ssafy.fundyou1.fund.entity.Funding;
+import com.ssafy.fundyou1.fund.entity.FundingItem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyFundingDto {
+    private Long id;
+    private Long endDate;
+    private boolean fundingStatus;
+    // 총가격
+    private int totalPrice;
+    // 현재 펀딩된 가격
+    private int currentFundingPrice;
+    private int percentage;
+    private List<FundingItem> fundingItemList;
+
+    public MyFundingDto(Funding funding, int totalPrice, int currentFundingPrice, int percentage, List<FundingItem> fundingItemList) {
+        this.id = funding.getId();
+        this.endDate = funding.getEndDate();
+        this.fundingStatus = funding.isFundingStatus();
+        this.totalPrice = totalPrice;
+        this.currentFundingPrice = currentFundingPrice;
+        this.percentage = percentage;
+        this.fundingItemList = fundingItemList;
+    }
+}
