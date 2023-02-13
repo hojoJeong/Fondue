188c93fd6eecf7a4eeed3e697013a3d8f86f3111
heejucherich <gmlwn1954@gmail.com>
2023-02-13 15:20:53 +0900

    refactor: #86 필요없는 코드 삭제

--- BE-develop/fundyou1/src/main/java/com/ssafy/fundyou1/like/dto/LikeRequestDto.java
+++ BE-develop/fundyou1/src/main/java/com/ssafy/fundyou1/like/dto/LikeRequestDto.java
@@ -1,12 +1,9 @@
 package com.ssafy.fundyou1.like.dto;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.validation.constraints.NotNull;
 
 
 //  찜에 담을 상품의 itemid를 전달받음
