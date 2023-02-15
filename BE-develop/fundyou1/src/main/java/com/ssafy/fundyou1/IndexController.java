986773f6ddadee27ef8c957458231affc4d1e916
Suyong <suyong5713@gmail.com>
2023-02-15 13:40:42 +0900

    feat: index.html

---
+++ BE-develop/fundyou1/src/main/java/com/ssafy/fundyou1/IndexController.java
@@ -0,0 +1,14 @@
+package com.ssafy.fundyou1;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public void index(){
+
+    }
+}
