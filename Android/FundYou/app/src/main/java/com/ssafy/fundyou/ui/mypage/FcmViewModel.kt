70377858c67df03fe7b9150b3f56c6f807c3a069
hojo0423 <hojo0423@naver.com>
2023-02-15 15:49:49 +0900

    feat: #68 푸쉬 알림 설정 구현

--- Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/mypage/FcmViewModel.kt
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/mypage/FcmViewModel.kt
@@ -7,24 +7,21 @@ import androidx.lifecycle.viewModelScope
 import com.ssafy.fundyou.common.ViewState
 import com.ssafy.fundyou.domain.usecase.fcm.GetNotificationSettingStatusUseCase
 import com.ssafy.fundyou.domain.usecase.fcm.UpdateNotificationSettingStatusUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class FcmViewModel @Inject constructor(
     private val getNotificationSettingStatusUseCase: GetNotificationSettingStatusUseCase,
     private val updateNotificationSettingStatusUseCase: UpdateNotificationSettingStatusUseCase,
 ) : ViewModel() {
-
-    private val _resultAddFcmToken = MutableLiveData<ViewState<String>>()
-    val resultAddFcmToken: LiveData<ViewState<String>>
-        get() = _resultAddFcmToken
-
     private val _notiStatus = MutableLiveData<ViewState<Int>>()
     val notiStatus: LiveData<ViewState<Int>>
         get() = _notiStatus
 
-    private val _resultUpdateNotiSetting = MutableLiveData<ViewState<String>>()
-    val resultUpdateNotiSetting: LiveData<ViewState<String>>
+    private val _resultUpdateNotiSetting = MutableLiveData<ViewState<Int>>()
+    val resultUpdateNotiSetting: LiveData<ViewState<Int>>
         get() = _resultUpdateNotiSetting
 
     fun getNotificationStatus() = viewModelScope.launch {
@@ -41,6 +38,7 @@ class FcmViewModel @Inject constructor(
         _resultUpdateNotiSetting.value = ViewState.Loading()
         try {
             updateNotificationSettingStatusUseCase()
+            _resultUpdateNotiSetting.value = ViewState.Success(0)
         } catch (e: Exception){
             _resultUpdateNotiSetting.value = ViewState.Error(e.message)
         }
--- Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/mypage/NotiSettingFragment.kt
+++ Android/FundYou/app/src/main/java/com/ssafy/fundyou/ui/mypage/NotiSettingFragment.kt
@@ -1,28 +1,78 @@
 package com.ssafy.fundyou.ui.mypage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.ssafy.fundyou.R
+import com.ssafy.fundyou.common.ViewState
 import com.ssafy.fundyou.databinding.FragmentNotiSettingBinding
 import com.ssafy.fundyou.ui.common.BaseFragment
 
 class NotiSettingFragment : BaseFragment<FragmentNotiSettingBinding>(R.layout.fragment_noti_setting) {
-
+    private val fcmViewModel by activityViewModels<FcmViewModel>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        initViewModels()
     }
     override fun initView() {
-        initSwtich()
+        fcmViewModel.getNotificationStatus()
     }
 
     override fun initViewModels() {
+        initNotiStatusObserve()
+        initResultUpdateNotiSettingObserve()
     }
 
-    private fun initSwtich(){
-        binding.swbtnNotiSettingAll.setOnCheckedChangeListener { buttonView, isChecked ->  
-            /** 알림 설정 서버 통신 */
+    private fun initNotiStatusObserve(){
+        fcmViewModel.notiStatus.observe(viewLifecycleOwner){ response ->
+            when(response){
+                is ViewState.Loading -> {
+                    Log.d(TAG, "initNotiStatusObserve: Get Notification Setting Status Loading...")
+                }
+                is ViewState.Success -> {
+                    Log.d(TAG, "initNotiStatusObserve: ${response.value}")
+                    when(response.value){
+                        0 -> {
+                            binding.swbtnNotiSetting.isChecked = false
+                        }
+                        1-> {
+                            binding.swbtnNotiSetting.isChecked = true
+                        }
+                    }
+                    initSwitchChangeListener()
+                }
+                is ViewState.Error -> {
+                    Log.d(TAG, "initResultUpdateNotiSettingObserve: Get Notification Setting Status Error...${response.message}")
+                }
+            }
         }
     }
 
+    private fun initSwitchChangeListener(){
+        binding.swbtnNotiSetting.setOnCheckedChangeListener { _, _ ->
+            fcmViewModel.updateNotificationStatus()
+        }
+    }
+
+    private fun initResultUpdateNotiSettingObserve(){
+        fcmViewModel.resultUpdateNotiSetting.observe(viewLifecycleOwner){ response ->
+            when(response){
+                is ViewState.Loading -> {
+                    Log.d(TAG, "initResultUpdateNotiSettingObserve: Update Notification Setting Status Loading...")
+                }
+                is ViewState.Success -> {
+                    Log.d(TAG, "initResultUpdateNotiSettingObserve: Update Notification Setting Status Success...")
+                }
+                is ViewState.Error -> {
+                    Log.d(TAG, "initResultUpdateNotiSettingObserve: Update Notification Setting Status Error...${response.message}")
+                }
+            }
+        }
+    }
+
+    companion object{
+        private const val TAG = "NotiSettingFragment..."
+    }
 }
